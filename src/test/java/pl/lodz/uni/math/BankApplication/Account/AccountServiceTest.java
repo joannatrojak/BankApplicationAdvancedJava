41d407c52103fcc57fda8b55734853a7f3abed56
Joanna <joasia405@gmail.com>
2019-07-13 16:23:17 +0200

    ifApproved

--- src/test/java/pl/lodz/uni/math/BankApplication/Account/AccountServiceTest.java
+++ src/test/java/pl/lodz/uni/math/BankApplication/Account/AccountServiceTest.java
@@ -37,7 +37,7 @@ class AccountServiceTest {
         accountTester.createNewAccount("regular");
         accountTester.sendAccountForApproval();
         accountTester.approveAccount();
-        //assertTrue(accountTester.ifApproved());
+        assertTrue(accountTester.ifApproved());
     }
     @Test
     void checkIfAccountNumberIsShownForTheAccount() throws Exception {
